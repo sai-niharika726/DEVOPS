43099fa8cf16690daeb3d822b32f0de3274c5f4d
sai-niharika726 <learningdevops45@gmail.com>
2026-05-20 21:51:34 +0530

    Update app.py

--- tasks/app.py
+++ tasks/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+VERSION = os.getenv("APP_VERSION", "v1")
+
 @app.route('/')
 def home():
-    return "Application Running"
+    return f"""
+    <html>
+        <head>
+            <title>Company App</title>
+        </head>
+        <body style="font-family:Arial; text-align:center; margin-top:100px;">
+            <h1>Flask Application Running Successfully</h1>
+            <h2>Application Version: {VERSION}</h2>
+        </body>
+    </html>
+    """
+
+@app.route('/health')
+def health():
+    return {
+        "status": "UP",
+        "version": VERSION
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
